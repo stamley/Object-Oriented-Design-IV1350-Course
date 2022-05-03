43eb08072db6b0cabbdb5c70753ae6ab65e9369a
stamley <axellystam@gmail.com>
2022-05-03 16:34:52 +0200

    Update Discountdatabase with three methods

--- src/test/se/kth/iv1350/hackers/integration/DiscountDatabaseTest.java
+++ src/test/se/kth/iv1350/hackers/integration/DiscountDatabaseTest.java
@@ -7,15 +7,30 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
+import se.kth.iv1350.hackers.DTO.DiscountDTO;
+import se.kth.iv1350.hackers.util.Amount;
+
+
 public class DiscountDatabaseTest {
+    private DiscountDatabase discountDatabase;
+
+    @BeforeEach
+    public void setupClass(){
+        DiscountDatabase discountDatabase = new DiscountDatabase();
+    }
+
+
     @Test
     public void testDiscountRequest() {
-
+        Amount amount = new Amount(0.1);
+        String typeOfDiscount = "PlaceholderTypeOfDiscount";
+        DiscountDTO expResult = new DiscountDTO(amount, typeOfDiscount);
+        DiscountDTO result = discountDatabase.discountRequest(100);
+        assertEquals(expResult, result, "The expected DiscountDTO did not correspond with the resulting DiscountDTO");
     }
 
     @Test
     public void testInvalidDiscount() {
-        DiscountDatabase discountDatabase = new DiscountDatabase();
         double result = discountDatabase.fetchDiscountAmountFromDatabase(100).getAmount();
         double validDisocunt = 1;
         assertTrue(result < validDisocunt , "The discount is not valid.");
@@ -23,6 +38,8 @@ public class DiscountDatabaseTest {
 
     @Test
     public void testFetchTypeOfDiscountFromDatabase() {
-
+        String expResult = "PlaceholderTypeOfDiscount";
+        String result = discountDatabase.fetchTypeOfDiscountFromDatabase(100);
+        assertEquals(expResult, result,"The method does not return the expected string.");
     }
 }
