c470c1575b9b32a9b6c7db5131f32f2763802688
stamley <axellystam@gmail.com>
2022-05-03 16:10:52 +0200

    Tests: DBController, DiscountDatabase, Cashregister
    Added test classes cashregister, dbcontroller and discountdatabase

--- src/test/se/kth/iv1350/hackers/integration/DBControllerTest.java
+++ src/test/se/kth/iv1350/hackers/integration/DBControllerTest.java
@@ -1,5 +1,26 @@
 package se.kth.iv1350.hackers.integration;
 
+import org.junit.Test;
+
 public class DBControllerTest {
-    
+    @Test
+    public void testDiscountRequest() {
+        DiscountDatabase discountDatabase = new DiscountDatabase();
+        
+    }
+
+    @Test
+    public void testGetItem() {
+
+    }
+
+    @Test
+    public void testRequestItemInfo() {
+
+    }
+
+    @Test
+    public void testUpdateExternalSystems() {
+
+    }
 }
--- src/test/se/kth/iv1350/hackers/integration/DiscountDatabaseTest.java
+++ src/test/se/kth/iv1350/hackers/integration/DiscountDatabaseTest.java
@@ -1,5 +1,28 @@
 package se.kth.iv1350.hackers.integration;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
 public class DiscountDatabaseTest {
-    
+    @Test
+    public void testDiscountRequest() {
+
+    }
+
+    @Test
+    public void testInvalidDiscount() {
+        DiscountDatabase discountDatabase = new DiscountDatabase();
+        double result = discountDatabase.fetchDiscountAmountFromDatabase(100).getAmount();
+        double validDisocunt = 1;
+        assertTrue(result < validDisocunt , "The discount is not valid.");
+    }
+
+    @Test
+    public void testFetchTypeOfDiscountFromDatabase() {
+
+    }
 }
