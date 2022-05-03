d6c05061a7d7ef11c3b83c7eb1622e523a0743cc
axellokrantz <axel.lokrantz@gmail.com>
2022-05-03 20:33:11 +0200

    Started working on ControllerTest

--- src/test/se/kth/iv1350/hackers/controller/ControllerTest.java
+++ src/test/se/kth/iv1350/hackers/controller/ControllerTest.java
@@ -1,5 +1,34 @@
 package se.kth.iv1350.hackers.controller;
 
+import org.junit.After;
+import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
+
+import se.kth.iv1350.hackers.DTO.SaleDTO;
+import se.kth.iv1350.hackers.integration.DBController;
+import se.kth.iv1350.hackers.integration.IOController;
+import se.kth.iv1350.hackers.util.Amount;
+import se.kth.iv1350.hackers.controller.Controller;
+
 public class ControllerTest {
+    private Controller controller;
+
+    @BeforeEach
+    public void setUp(){
+        Controller controller = new Controller(new IOController(), new DBController());
+    }
     
+    @After
+    public void tearDown(){
+        controller = null;
+    }
+
+    @Test
+    public void addItemTest(){
+        String itemIdentifier = "12345";
+        Amount quantity = new Amount(5);
+
+        SaleDTO result = controller.addItem(itemIdentifier, quantity);
+    }
+
 }
--- src/test/se/kth/iv1350/hackers/util/AmountTest.java
+++ src/test/se/kth/iv1350/hackers/util/AmountTest.java
@@ -2,7 +2,6 @@ package se.kth.iv1350.hackers.util;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
-import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
 public class AmountTest {
