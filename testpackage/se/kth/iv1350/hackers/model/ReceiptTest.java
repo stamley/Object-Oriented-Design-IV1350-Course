df76aa9cb721ddb849d3581b6be35c5227d8be67
Iwilkens <90352345+Iwilkens@users.noreply.github.com>
2022-05-03 15:15:38 +0200

    Update ReceiptTest.java

--- testpackage/se/kth/iv1350/hackers/model/ReceiptTest.java
+++ testpackage/se/kth/iv1350/hackers/model/ReceiptTest.java
@@ -1,6 +1,29 @@
 package testpackage.se.kth.iv1350.hackers.model;
 
-public void testReceiptToString() {
+
+import sourcepackage.se.kth.iv1350.hackers.model.*;
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import sourcepackage.se.kth.iv1350.hackers.DTO.*;
+import sourcepackage.se.kth.iv1350.hackers.util.Amount;
+import org.junit.Test;
+
+public class ReceiptTest{
+
+    @Test
+    public void testReceiptToString() {
+        TotalPrice totalPrice = new TotalPrice();
+
+        LocalDateTime localDateTime = LocalDateTime.now();
+
+
     
     
+    }
 }
+
+private final TotalPrice totalPrice;
+private final LocalDateTime localDateTime;
+private HashMap <String, Item> items = new HashMap<String, Item>();
+private Amount amountPaid;
+private Amount changeAmount;
