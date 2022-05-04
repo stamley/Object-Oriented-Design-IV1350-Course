1c81a2ac5e344c63527ae61f6841d2bbac430e59
stamley <axellystam@gmail.com>
2022-05-04 11:42:46 +0200

    Controller + Sale
    Changed endSale and register payment to behave correctly.
    Changed registerpayment to behave correctly.

--- src/main/se/kth/iv1350/hackers/controller/Controller.java
+++ src/main/se/kth/iv1350/hackers/controller/Controller.java
@@ -64,24 +64,27 @@ public class Controller {
     }
 
     /**
-     * Finalizes the sale.
+     * Ends the sale, creates and prints receipt and updates external systems.
      * 
      * @return The final version of SaleDTO containing all sale information.
      */
     public SaleDTO endSale(){
-        return currentSale.endSale();
+        //return currentSale.endSale();
+        SaleDTO endedSaleDTO = currentSale.endSale();
+        currentReceipt = new Receipt(endedSaleDTO);
+        dbController.updateExternalSystems(endedSaleDTO);
+        printReceipt();
+        return endedSaleDTO;
     }
 
     /**
-     * Registers the customer payment which will create a receipt return the change.
+     * Registers the customer payment which will update the current sale.
      * 
      * @param payment Customer payment.
      * @return Change of sale as an amount.
      */
     public Amount registerPayment(Amount payment){
-        SaleDTO endedSaleDTO = currentSale.registerPayment(payment);
-        currentReceipt = new Receipt(endedSaleDTO);
-        return endedSaleDTO.getChangeAmount();
+        return currentSale.registerPayment(payment);
     }
 
     /**
