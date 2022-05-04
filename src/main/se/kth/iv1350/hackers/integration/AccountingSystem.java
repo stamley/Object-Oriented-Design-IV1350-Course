b0bf86bc304a767de9589ac8b02529da19d9772f
axellokrantz <axel.lokrantz@gmail.com>
2022-05-04 16:30:33 +0200

    Updated AccountingSystem
    La till en arraylist.

--- src/main/se/kth/iv1350/hackers/integration/AccountingSystem.java
+++ src/main/se/kth/iv1350/hackers/integration/AccountingSystem.java
@@ -1,10 +1,17 @@
 package se.kth.iv1350.hackers.integration;
 
+import java.util.ArrayList;
 import se.kth.iv1350.hackers.DTO.SaleDTO;
 
+/**
+     * Logs the current sale in the accounting system.
+     * 
+     * @param saleInformation SaleDTO containing information of sale.
+     */
 public class AccountingSystem {
-    
-    public void updateAccountingSystem (SaleDTO saleinformation){
-        
+    private ArrayList<SaleDTO> accountingSystem = new ArrayList<SaleDTO>();
+
+    public void updateAccountingSystem (SaleDTO saleInformation){
+        accountingSystem.add(saleInformation);
     }
 }
