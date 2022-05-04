c5be8d6857cb00a451315f8453112317fccf8ad1
axellokrantz <axel.lokrantz@gmail.com>
2022-05-04 15:16:02 +0200

    någon update

--- src/test/se/kth/iv1350/hackers/controller/ControllerTest.java
+++ src/test/se/kth/iv1350/hackers/controller/ControllerTest.java
@@ -3,32 +3,49 @@ package se.kth.iv1350.hackers.controller;
 import org.junit.After;
 import org.junit.Test;
 import org.junit.jupiter.api.BeforeEach;
+import static org.junit.jupiter.api.Assertions.*;
 
-import se.kth.iv1350.hackers.DTO.SaleDTO;
-import se.kth.iv1350.hackers.integration.DBController;
-import se.kth.iv1350.hackers.integration.IOController;
+import se.kth.iv1350.hackers.DTO.*;
+import se.kth.iv1350.hackers.integration.*;
 import se.kth.iv1350.hackers.util.Amount;
-import se.kth.iv1350.hackers.controller.Controller;
+import se.kth.iv1350.hackers.model.*;
+
 
 public class ControllerTest {
     private Controller controller;
+    private Sale currentSale;
     private SaleDTO saleDTO;
 
     @BeforeEach
     public void setUp(){
         controller = new Controller(new IOController(), new DBController());
+        currentSale = new Sale();
+        saleDTO = new SaleDTO(currentSale);
     }
     
     @After
     public void tearDown(){
         controller = null;
+        saleDTO = null;
     }
 
     @Test
     public void addItemTest(){
-        String itemIdentifier = "12345";
-        Amount quantity = new Amount(5);
-        saleDTO = controller.addItem(itemIdentifier, quantity);
+
+        String itemName = "Tomato";
+        Amount costOfItem = new Amount(5);
+        Amount VATOfItem = new Amount(20);
+        Item itemToAdd = new Item(new ItemDTO(itemName, costOfItem, VATOfItem), "1122", new Amount(2));
+        
+        Amount quantity = new Amount (5);
+        saleDTO = controller.addItem("1122", quantity);
+
+        boolean expectedResult = true;
+        String first = saleDTO.getItemList().get("1122").getItemDescription().getItemName();
+        String second = itemToAdd.getItemIdentifier();
+        boolean result = first.equals(second);
+        
+        assertEquals(expectedResult, result, "");
     }
 
 }
--- src/test/se/kth/iv1350/hackers/model/SaleTest.java
+++ src/test/se/kth/iv1350/hackers/model/SaleTest.java
@@ -1,9 +1,6 @@
 package se.kth.iv1350.hackers.model;
 
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
