a362c39ae6f3462c345eaf2c46dfc6a4770fc57f
Iwilkens <90352345+Iwilkens@users.noreply.github.com>
2022-05-17 09:06:01 +0200

    Kommentarer för exceptionklasser
    Skrivit kommentarer för nya exceptionklassser

--- src/main/se/kth/iv1350/hackers/integration/InventorySystemException.java
+++ src/main/se/kth/iv1350/hackers/integration/InventorySystemException.java
@@ -3,6 +3,12 @@ package se.kth.iv1350.hackers.integration;
  * This exception is thrown when the Inventory System is not functioning, for example when the server is down.
  */
 public class InventorySystemException extends RuntimeException {
+
+    /**
+     *  A new instance created representing the condition in the message
+     * 
+     * @param message The message explaining what caused the exception.
+     */
     
     public InventorySystemException (String message){
         super(message);
